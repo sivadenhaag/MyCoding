384ab08d96153d7c1624d6ec9a4ce840e3dc1fbb
Sivanarayanan Pachaiyappan <siva@Sivanarayanans-MacBook-Pro.local>
2018-07-17 10:18:38 +0200

    Completion of Excercise 2

--- Excercise2.py
+++ Excercise2.py
@@ -9,27 +9,25 @@ total_capacity = 0
 total_stoptime = 0
 
 csv_data = {}
-
-for details in data ['events_data']:
-
-	longitude	= (details['geo_point']['longitude'])
-	latitude	= (details['geo_point']['latitude'])
-	stoptime 	= (details['algorithm_fields']['stop_time'])
-	capacity 	= (details['algorithm_fields']['capacity'])
+with open ("Output.csv","w") as csvFile:
+	writer = csv.writer(csvFile)
+	writer.writerow(['Longitude','Latitude','Stoptime','Capacity'])
 	
-	#print ('Longitude :',longitude, ' ','Latitude :', latitude, ' ','StopTime : ',stoptime, ' ','Capacity :', capacity)
+	for details in data ['events_data']:
 
-	csv_data['longitude'] = longitude
-	csv_data['latitude'] = latitude
-	csv_data['stoptime'] = stoptime
-	csv_data['capacity'] = capacity
+		longitude	= (details['geo_point']['longitude'])
+		latitude	= (details['geo_point']['latitude'])
+		stoptime 	= (details['algorithm_fields']['stop_time'])
+		capacity 	= (details['algorithm_fields']['capacity'])
+		
+		#print ('Longitude :',longitude, ' ','Latitude :', latitude, ' ','StopTime : ',stoptime, ' ','Capacity :', capacity)
 
-	print(csv_data)
+		csv_data['longitude'] = longitude
+		csv_data['latitude'] = latitude
+		csv_data['stoptime'] = stoptime
+		csv_data['capacity'] = capacity
 
-	with open ("Output.csv","w") as csvFile:
-		writer = csv.writer(csvFile)
-		writer.writerow(['Longitude','Latitude','Stoptime','Capacity'])
-		for row in csv_data:
-			writer.writerow(row)
+		writer.writerow(csv_data.values())
 
+		#print(csv_data)
 
